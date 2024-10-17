9c37d5f4532924cce3f8390a17e58d7b6a8c1c34
maushamkumar <maushamroy143@gmail.com>
2024-10-17 20:29:25 +0530

    initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning to predict trip duration for tais in NYC',
+    author='Mausham Kumar',
+    license='MIT',
+)
